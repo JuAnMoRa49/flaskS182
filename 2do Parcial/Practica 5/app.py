0f4404fe47762c606ca19334da9cfc053d8c8493
JuAnMoRa49 <125929793+JuAnMoRa49@users.noreply.github.com>
2023-06-29 18:23:03 -0600

    Practica 7

--- 2do Parcial/Practica 5/app.py
+++ 2do Parcial/Practica 5/app.py
@@ -2,7 +2,8 @@
 
 # importacion de flask
 from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_mysqldb import MySQL
+# importacion de mysql
+# from flask_mysqldb import MySQL
 
 # se declara app y se asigna un nombre, es decir, es la inicialización del servidor Flask
 app = Flask(__name__)
@@ -17,7 +18,7 @@ app.config['MYSQL_DB']="dbflask"
 
 app.secret_key='mysicretkey'
 
-mysql= MySQL(app)
+
 
 # se declaró la ruta
 # se declaró la ruta Index, o la ruta principal, http://localhost:5000
@@ -27,6 +28,13 @@ mysql= MySQL(app)
 #si la ruta existe, regrela index
 def index():
     return render_template('index.html')
+    curSelect= mysql.connection.cursor()
+    curSelect.execute('select * from Albums')
+    consulta= curSelect.fetchall()
+    # print(consulta)
+    
+    return render_template('index.html', listaAlbums=consulta)
+    
 
 
 # para poder ejecutar esta funcion, en el link se le agrega /guardar
@@ -39,11 +47,11 @@ def guardar():
         Vanio = request.form['txtAnio']
         #print(titulo, artista, anio)
         
-        #variable de tipo cursor para almacenar la informacion
-        CS= mysql.connection.cursor()
-        # es el query que se ejecutará para ingresar los datos en la base de datos
-        CS.execute('insert into Albums (titulo, artista, anio) values(%s, %s, %s)',(Vtitulo, Vartista, Vanio))
-        mysql.connection.commit()
+        # #variable de tipo cursor para almacenar la informacion
+        # CS= mysql.connection.cursor()
+        # # es el query que se ejecutará para ingresar los datos en la base de datos
+        # CS.execute('insert into Albums (titulo, artista, anio) values(%s, %s, %s)',(Vtitulo, Vartista, Vanio))
+        # mysql.connection.commit()
         
     flash('Album agregado correctamente')
     return redirect(url_for('index'))
